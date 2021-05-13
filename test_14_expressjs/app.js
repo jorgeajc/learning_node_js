330eb4d7b5bf65dd9f2388b8aff35abfc16b9612
JorgeJC98 <69093957+JorgeJC98@users.noreply.github.com>
2021-05-12 19:31:55 -0600

    refactory directory proyect web page

--- test_14_expressjs/app.js
+++ test_14_expressjs/app.js
@@ -1,14 +1,37 @@
+const path = require('path')
 const express = require('express')
 
-
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '/public')
 
-app.get('', (req, res)=>{
-    res.send('hello')
+
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
 })
 
-app.get('/help', (req, res)=>{
-    res.send('help')
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
 })
 
 app.listen(3000, ()=>{
