cb472a93a0cb0e02aaa820671a62388375f25ffb
JorgeJC98 <69093957+JorgeJC98@users.noreply.github.com>
2021-05-13 10:03:42 -0600

    config web server with expressjs

--- test_14_expressjs/app.js
+++ test_14_expressjs/app.js
@@ -1,36 +1,54 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '/public')
+const publicPath = path.join(__dirname, './public')
+const viewsPath = path.join(__dirname, './resources/views')
+const partialsPath = path.join(__dirname, './resources/partials')
 
-
+// setup handlersbars engine ande vies location
 app.set('view engine', 'hbs')
-app.use(express.static(publicDirectoryPath))
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// setup statis directoy to server
+app.use(express.static(publicPath))
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Jorge'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Jorge'
     })
 })
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.'
+        title: 'help',
+        helpText: 'This is some helpful text.',
+        name: 'Jorge'
     })
 })
 
-app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'Error 404',
+        name: 'jorge',
+        errorMsg: 'This Article Not Found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Error 404',
+        name: 'jorge',
+        errorMsg: 'This Page Not Found'
     })
 })
 
